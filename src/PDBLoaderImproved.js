4c26cc53b63edab09ad2e26ae7835b549a463569
tlskillman <tlskillman@gmail.com>
2016-08-01 20:55:28 -0700

    Update PDBLoaderImproved.js

--- src/PDBLoaderImproved.js
+++ src/PDBLoaderImproved.js
@@ -173,7 +173,7 @@
 				// compute offset
 				var offset = max.clone().add(min).multiplyScalar(0.5);
 
-				var stick = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 1), new THREE.MeshBasicMaterial({color: 0xff0000}));
+				var stick = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, 1), new THREE.MeshBasicMaterial({color: 0xffffff}));
 				var bonds = new THREE.Mesh(new THREE.Geometry(), stick.material);
 				bonds.name = 'bonds';
 				if(options.mergeLikeAtoms)
@@ -246,7 +246,7 @@
 							var start = mesh.position, end = v2;
 							stick.position.copy( start );
 							stick.position.lerp( end, 0.5 );
-							stick.color=0xff0000;
+							stick.material.color=0x00ff00;
 							stick.scale.setZ( start.distanceTo( end ) );
 							stick.lookAt( end );
 
